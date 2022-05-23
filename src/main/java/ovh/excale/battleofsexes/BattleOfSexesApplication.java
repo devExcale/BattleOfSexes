67d12478fb0a4c228a27a7781cf4b2346bb8bbc2
Emanuele Scaccia <dev_excale@hotmail.com>
2022-05-23 09:13:50 +0200

    Init commit

---
+++ src/main/java/ovh/excale/battleofsexes/BattleOfSexesApplication.java
@@ -0,0 +1,13 @@
+package ovh.excale.battleofsexes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleOfSexesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleOfSexesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ovh/excale/battleofsexes/BattleOfSexesApplicationTests.java
@@ -0,0 +1,13 @@
+package ovh.excale.battleofsexes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleOfSexesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
